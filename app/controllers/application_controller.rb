d332d547357122b89e08fe00aeb42eda5be1a36d
Ayana Bando <ayanaflax@yahoo.com>
2019-02-14 19:00:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def current_user
+    redirect_to controller: 'sessions', action: 'new' unless session[:name]
   end
 end

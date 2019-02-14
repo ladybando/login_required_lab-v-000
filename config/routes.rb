2034ae25cce8de93dbc4de6975c7726907e063ab
Ayana Bando <ayanaflax@yahoo.com>
2019-02-14 19:15:51 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
     get 'show/' => 'secrets#show'
+    get 'new/' => 'sessions#new'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase

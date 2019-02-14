01cd13ac22bfd2b1f81b1076a1d5d9c2cc8ab014
Ayana Bando <ayanaflax@yahoo.com>
2019-02-14 19:29:46 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,9 +1,6 @@
 class SecretsController < ApplicationController
   before_action :require_login
   
-  def index
-  end
-  
   def show
     redirect_to controller: 'sessions', action: 'new' unless session[:name]
   end
